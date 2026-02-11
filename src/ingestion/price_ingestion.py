9572371ff5196dd4422e597d3e7288c0ca26c03d
Rifqi Adlan Apriyadi <adilapriyadi@yahoo.com.sg>
2026-02-11 16:59:22 +0900

    Add logging to ingestion

--- src/ingestion/price_ingestion.py
+++ src/ingestion/price_ingestion.py
@@ -1,5 +1,6 @@
 """The module containing the ingestion function."""
 
+import logging
 from typing import Iterable, List
 
 from sqlalchemy.dialects import postgresql
@@ -16,10 +17,19 @@ CLIENTS_TO_CALL: Iterable[clients.CryptoClient] = {
 
 def ingest_prices(assets: Iterable[str]) -> None:
     """Fetch price data by calling clients and push the data to the database."""
+    logging.info("Running price ingestion for %s with %s", assets, CLIENTS_TO_CALL)
+
     db = next(database.get_db())
     prices: List[schemas.BaseCryptoPrice] = []
+
+    logging.info("Fetching prices from clients.")
     for client in CLIENTS_TO_CALL:
-        prices = prices + client.get_prices(assets)
+        try:
+            logging.info("Fetching prices of %s from %s", assets, client)
+            prices = prices + client.get_prices(assets)
+        except Exception as e:  # pylint: disable=broad-exception-caught
+            logging.error("An error occurred while calling %s: %s", client, repr(e))
+            raise e
 
     # Cannot do bulk upsert using the ORM as the ingestion upsert needs to use
     # the model's unique constraint on asset, quote, and source
@@ -34,6 +44,7 @@ def ingest_prices(assets: Iterable[str]) -> None:
         for price in prices
     ]
 
+    logging.info("Upserting %s", rows)
     stmt = postgresql.insert(models.CryptoPrice).values(rows)
 
     stmt = stmt.on_conflict_do_update(
@@ -46,6 +57,7 @@ def ingest_prices(assets: Iterable[str]) -> None:
 
     db.execute(stmt)
     db.commit()
+    logging.info("Changes pushed to the database.")
 
 
 if __name__ == "__main__":
